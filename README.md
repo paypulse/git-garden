267e96e1664b68f3ab31eb22814f1af61b515abe
= <=>
2025-04-29 14:01:27 +0900

    Add Readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Git Repository 별 commit 을 3D 객체로 표현 하기 
+- git hub Action을 사용 하고 있음
+- [확인] :   https://paypulse.github.io/git-garden/
--- garden.js
+++ garden.js
@@ -4,7 +4,15 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xa3d5f7);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 20;
+camera.position.set(3,3,3);
+camera.lookAt(0,0,0);
+//camera.position.z = 20;
+//camera.position.x =
+
+//const camera = new THREE.CubeCamera(1, 1000, 256);
+//camera.position.set(0,0,0);
+
+//const camera = new THREE.StereoCamera();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
