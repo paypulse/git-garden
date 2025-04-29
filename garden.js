ed1c69c791cbeb11973359450730d8260ac7a7cb
= <=>
2025-04-29 13:48:36 +0900

    change camera

--- garden.js
+++ garden.js
@@ -4,7 +4,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xa3d5f7);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 20;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
